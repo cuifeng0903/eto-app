50ed5faf5c85d55f81f927cec1ba7377bc2a44ce
cuifeng0903 <cuifeng0903@gmail.com>
2025-12-03 23:05:46 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'eto-app-v3';
+const urlsToCache = [
+    '/', '/index.html', '/style.css', '/script.js', '/manifest.json',
+    '/images/001.png','/images/002.png','/images/003.png','/images/004.png',
+    '/images/005.png','/images/006.png','/images/007.png','/images/008.png',
+    '/images/009.png','/images/010.png','/images/011.png','/images/012.png',
+    '/images/icon-192.png', '/images/icon-512.png', '/images/icon.png'
+];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
