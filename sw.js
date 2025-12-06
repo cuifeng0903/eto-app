05ba33b92ce243e2e269618990e446d4596fc868
cuifeng0903 <cuifeng0903@gmail.com>
2025-12-06 18:34:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-const CACHE_NAME = 'eto-app-v3';
+const CACHE_NAME = 'eto-app-v1';
 const urlsToCache = [
-    '/', '/index.html', '/style.css', '/script.js', '/manifest.json',
-    '/images/001.png','/images/002.png','/images/003.png','/images/004.png',
-    '/images/005.png','/images/006.png','/images/007.png','/images/008.png',
-    '/images/009.png','/images/010.png','/images/011.png','/images/012.png',
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/images/001.png', '/images/002.png', '/images/003.png', '/images/004.png',
+    '/images/005.png', '/images/006.png', '/images/007.png', '/images/008.png',
+    '/images/009.png', '/images/010.png', '/images/011.png', '/images/012.png',
     '/images/icon-192.png', '/images/icon-512.png', '/images/icon.png'
 ];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
